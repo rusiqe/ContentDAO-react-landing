48647d1bb4648d24e8af1563d33782a7f7f4be85
Taurai. M <rusiqe@gmail.com>
2022-10-12 08:00:57 +0200

    icons

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
-import Image from '../../elements/Image';
+//import { Link } from 'react-router-dom';
+//import Image from '../../elements/Image';
 
 const Logo = ({
   className,
@@ -18,15 +18,9 @@ const Logo = ({
       {...props}
       className={classes}
     >
-      <h1 className="m-0">
-        <Link to="/">
-          <Image
-            src={require('./../../../assets/images/logo.svg')}
-            alt="Open"
-            width={32}
-            height={32} />
-        </Link>
-      </h1>
+      <h3 className="m-0">
+        Content_DAO
+      </h3>
     </div>
   );
 }
